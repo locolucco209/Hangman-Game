768f79b2f0d50d0897a80de3ca118d126f172504
Luke Massetti <lukemassetti@WestSide-Luke.local>
2018-02-16 22:44:46 -0800

    new JS file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,36 @@
-    var randomWords = ["address", "altcoin", "asicminer", "blockchain", "block", "blockheight", "blockreward", "distributed&centralledger", "fork", "halving", "hashrate", "mining", "multisig", "node", "p2p", "pow", "pos", "publicprivatekey", "signature", "smartcontract"];
-    var randoWu = randomWords[Math.floor(Math.random() * randomWords.length)];
+var randomWords = ["address", "altcoin", "asicminer", "blockchain", "block", "blockheight", "blockreward", "distributed&centralledger", "fork", "halving", "hashrate", "mining", "multisig", "node", "p2p", "pow", "pos", "publicprivatekey", "signature", "smartcontract"];
+var randoWu = randomWords[Math.floor(Math.random() * randomWords.length)];
+var s;
+var count = 0;
+var answerArray = [];
 
-    var s;
-    var count = 0;
-    var answerArray = [];
+function startUp() {
+    for (var i = 0; i < randoWu.length; i++) {
+        answerArray[1] = "_";
+    }
+    s = answerArray.join(" ");
+    document.getElementById("answer").innerHTML = s;
+}
 
-    function startUp() {
+function Letter() {
+    var letter = document.getElementById("Letter").value;
+    document.getElementByID('letter').addEventListener('click', function() {});
+    if (letter.length > 0) {
         for (var i = 0; i < randoWu.length; i++) {
-            answerArray[1] = "_";
-        }
-
-        s = answerArray.join(" ");
-        document.getElementById("answer").innerHTML = s;
-        document.getElementByID('letter').addEventListener('click', function() {});
-    }
-
-    function Letter() {
-        var letter = document.getElementById("Letter").value;
-        document.getElementByID('letter').addEventListener('click', function() {});
-
-        if (letter.length > 0) {
-            for (var i = 0; i < randoWu.length; i++) {
-                if (randoWu[i] === letter) {
-                    answerArray[i] === letter;
-                }
+            if (randoWu[i] === letter) {
+                answerArray[i] === letter;
             }
-
-            count++;
-            document.getElementById("counter").innerHTML = "No of clicks: " + count;
-            document.getElementById("answer").innerHTML = answerArray.join(" ");
         }
+        count++;
+        document.getElementById("counter").innerHTML = "Number of clicks: " + count;
+        document.getElementById("answer").innerHTML = answerArray.join(" ");
     }
+}
+
+document.getElementByID('Letter').addEventListener('click', function() {
+    //Code you want to happen on user clicks
+    Letter();
+});
+
+//Stuffs that happen when the window/browswer is done loading stuffs
+startUp()
